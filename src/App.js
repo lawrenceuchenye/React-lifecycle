a1ae8e179d2f9ee3f739b622cf120583abe9ea53
lawrence_k_uchenye <lawuche29@gmail.com>
2022-09-08 18:37:35 +0100

    Did some commonly used life cycle methods

--- src/App.js
+++ src/App.js
@@ -7,26 +7,52 @@ class App extends React.Component{
   constructor(props){
       super(props);
       this.state={
-          counter:5
+          counter:5,
+          mount:true
       }
+
       console.log("Constructor");
-      this.increment=()=>this.setState({counter:this.state.counter+1});
-      this.decrement=()=>this.setState({counter:this.state.counter-1});
+      this.increment=()=>this.setState({counter:this.state.counter+1,mount:this.state.mount});
+      this.decrement=()=>this.setState({counter:this.state.counter-1,mount:this.state.mount});
+
+      this.toggle=()=>this.setState({counter:this.state.counter,mount:!this.state.mount});
   }
   
+  componentDidMount(){
+      console.log("Component Did Mount");
+      console.log("+------------------+");
+  }
 
   render(){
       console.log("Render");
       return(
           <div className="App">
-             <h1>Count: {this.state.counter}</h1>
              <div>
-                 <button onClick={()=>this.increment()}> ADD </button>
-                 <button onClick={()=>this.decrement()}> MINUS </button>
+               <button onClick={()=>this.toggle()} disabled={this.state.mount}> MOUNT </button>
+                <button onClick={()=>this.toggle()} disabled={!this.state.mount}> UNMOUNT </button>
                </div>
-            </div>
+
+          { this.state.mount && (<div>
+                 <h1>Count: {this.state.counter}</h1>
+                 <div>
+                     <button onClick={()=>this.increment()}> ADD </button>
+                      <button onClick={()=>this.decrement()}> MINUS </button>
+                   </div>
+                  </div>)}
+                </div>
       );
   }
+
+  componentDidUpdate(prevProps,prevState,snapshots){
+      console.log("Component Did Update");
+      console.log("+------------------+");
+  }
+
+  componentWillUnmount(){
+      console.log("Component Will Unmount");
+      console.log("+---------------------+");
+  }
+
 }
 
 export default App; 
